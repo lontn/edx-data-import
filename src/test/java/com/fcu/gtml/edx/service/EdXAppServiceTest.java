a8412b5b955492879ddc3d85a722a84e396e5c16
lontn <lontn@hotmail.com>
2017-11-27 23:57:54 +0800

    test

--- src/test/java/com/fcu/gtml/edx/service/EdXAppServiceTest.java
+++ src/test/java/com/fcu/gtml/edx/service/EdXAppServiceTest.java
@@ -1,11 +1,15 @@
 package com.fcu.gtml.edx.service;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.fcu.gtml.edx.SupportSpringTest;
 
@@ -17,5 +21,11 @@ public class EdXAppServiceTest extends SupportSpringTest {
     @Test
     public void test() {
         L.info("mongoOperation:{}", mongoOperation);
+        Criteria syncCriteria = new Criteria();
+        Query query = Query.query(syncCriteria).limit(500);
+        List<String> list = mongoOperation.find(query, String.class);
+        L.info("list:{}", list.get(0));
+//        query.with(new Sort(Sort.Direction.ASC, "_id"));
+//        mongoOperation.getCollection("modulestore");
     }
 }
